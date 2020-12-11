ce5e5e2824c4c1699d009c55c4d9944cc59791a3
jopatino <jopatino8256@gmail.com>
2020-12-11 10:03:18 -0500

    Mi primer plugin para generar AutoPlay en mi reporductor. Además de ello se agregó función para silenciar video

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,9 @@
 import MediaPlayer from './MediaPlayer.js';
-
+import AutoPlay from './plugins/AutoPlay.js'
 const video = document.querySelector("video")
-const button = document.querySelector("button")
+const playPauseButton = document.querySelector("#play_pause")
+const muteUnmuteButton = document.querySelector("#mute_unmute")
 
-const player = new MediaPlayer({ el: video })
-button.onclick = () => player.toggle();
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] })
+playPauseButton.onclick = () => player.togglePlay();
+muteUnmuteButton.onclick = () => player.toggleMute();
